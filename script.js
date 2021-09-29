353d88cf71ecc4ef8ee7655ff7c6f23c3be14ea3
msmanawadu <msmanawadu@gmail.com>
2021-09-29 15:12:53 +0530

    Fetching Data from Quote API

---
+++ script.js
@@ -0,0 +1,25 @@
+// Array of Quote Objects
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+// Get Quotes from the API
+async function getQuotes() {
+    const apiURL = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiURL);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // Catch error here
+        console.log(error);
+    }
+}
+
+// On Load the Script
+getQuotes();
